9f14775a2bb2eb5b7fb4ac27a07dacd0f3d096db
Dan Gottlieb <dan@gotdan.org>
2015-11-18 10:59:44 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+var express = require("express")
+var cors    = require("cors")
+var path    = require("path")
+var smartAuth    = require("./smart-auth")
+var reverseProxy = require ("./reverse-proxy")
+
+var port = (process.argv[2] || "3000")
+
+var config = {
+	fhirServer: "https://fhir-open-api-dstu2.smarthealthit.org",
+	baseUrl: `http://localhost:${port}/smart`,
+	jwtSecret: "thisisasecret"
+}
+
+var app = express()
+
+app.use( cors() )
+
+//web page to kick things off
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client.html"))
+})
+
+//stubs smart oauth requests
+app.use( "/smart", smartAuth(config) )
+
+//reverse proxies requests to config.fhirServer and fixes urls
+app.use( "/smart", reverseProxy(config) )
+
+module.exports = app
+
+if (!module.parent) {
+	app.listen(port)
+	console.log(`Proxy server running on localhost on port ${port}`)
+}
