6e994e1fcdcdf7ac7509a3ecdf7ac50f60237b1c
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2016-01-26 13:08:03 -0500

    Handle XML conformance statement requests

--- src/smart-auth.js
+++ src/smart-auth.js
@@ -2,34 +2,85 @@ var jwt        = require("jsonwebtoken")
 var bodyParser = require("body-parser")
 var router     = require("express").Router()
 var request    = require('request')
+var xml2js     = require('xml2js')
 
 module.exports = (config) => {
 
 	router.get("/metadata", (req, res) => {
 
 		var url = config.fhirServer + "/metadata"
+        var acceptsJSON = req.headers.accept.indexOf('json') >= 0
 
-		request({
-		    url: url,
-		    json: true
-		}, function (error, response, body) {
-		    if (!error && response.statusCode === 200) {
-		    	var conformance = body
-		        //TODO: handle xml metadata requests
-				conformance.rest[0].security['extension'] = [{
-			      "url": "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris",
-				  "extension": [{
-			        "url": "authorize",
-			        "valueUri": config.baseUrl + "/authorize"
-			      },{
-			        "url": "token",
-			        "valueUri": config.baseUrl + "/token"
-			      }]
-				}]
-				res.type("application/json+fhir")
-				res.send(conformance)
-		    }
-		})
+        if (acceptsJSON) {
+			request({
+			    url: url,
+			    json: true
+			}, (error, response, body) => {
+			    if (!error && response.statusCode === 200) {
+			    	var conformance = body
+					conformance.rest[0].security['extension'] = [{
+				      "url": "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris",
+					  "extension": [{
+				        "url": "authorize",
+				        "valueUri": config.baseUrl + "/authorize"
+				      },{
+				        "url": "token",
+				        "valueUri": config.baseUrl + "/token"
+				      }]
+					}]
+					res.type("application/json+fhir")
+					res.send(conformance)
+			    }
+			})
+		} else {
+			request(url, (error, response, body) => {
+			    if (!error && response.statusCode === 200) {
+					var parseString = xml2js.parseString
+					parseString(body, (err, result) => {
+					    result.Conformance.rest[0].security[0]['extension'] = [
+					      {
+					         "$": {
+					            "url": "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris"
+					         },
+					         "extension": [
+					            {
+					               "$": {
+					                  "url": "authorize"
+					               },
+					               "valueUri": [
+					                  {
+					                     "$": {
+					                        "value": config.baseUrl + "/authorize"
+					                     }
+					                  }
+					               ]
+					            },
+					            {
+					               "$": {
+					                  "url": "token"
+					               },
+					               "valueUri": [
+					                  {
+					                     "$": {
+					                        "value": config.baseUrl + "/token"
+					                     }
+					                  }
+					               ]
+					            }
+					         ]
+					      }
+					   ]
+
+					    var builder = new xml2js.Builder()
+					    var xml = builder.buildObject(result)
+
+					    res.type("application/xml+fhir")
+					    res.send(xml)
+					})
+			    }
+			})
+		}
+
 	})
 
 	router.get("/authorize", (req, res) => {
